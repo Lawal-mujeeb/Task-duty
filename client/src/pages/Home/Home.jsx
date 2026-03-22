ec2f821270f463752830d533c3e28f5bb90c0842
APPLE <mujeeblawal357@gmail.com>
2026-03-22 09:56:12 +0100

    first commit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router";
+
 
 export default function Home() {
   return (
     <>
-      <main className="w-full bg-white min-h-screen flex items-center">
-        <div className="container mx-auto grid grid-cols-12 px-4 lg:px-0   ">
+      <main className=" w-full bg-white min-h-screen flex items-center">
+        <div className="container mx-auto grid grid-cols-12 px-4 lg:px-20   ">
           <div className="col-span-12 lg:col-span-6 flex flex-col justify-center gap-6">
             <h1 className="text-4xl font-bold mb-4 w-[70%]">
               Manage your Task on{" "}
@@ -18,9 +18,12 @@ export default function Home() {
               voluptates! Ullam quis natus aperiam aspernatur voluptatum aliquid
               asperiores exercitationem? Reprehenderit!
             </p>
-            <button className="border-2 border-blue-600 text-blue-600 px-6 py-2 rounded hover:bg-blue-600 hover:text-white transition md:w-[30%] md:mx-auto lg:mx-0">
-              <Link to="/alltask">Go to my task</Link>
-            </button>
+            <a
+  href="/alltask"
+  className="inline-block border-2 border-blue-600 text-blue-600 px-6 py-2 rounded hover:bg-blue-600 hover:text-white transition md:w-[30%] md:mx-auto lg:mx-0 text-center"
+>
+  Go to my task
+</a>
           </div>
 
           <div className="col-span-12 lg:col-span-6 flex lg:justify-end md:justify-center items-center mt-20 lg:mt-0">

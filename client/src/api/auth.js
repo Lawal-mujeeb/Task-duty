2736448188c93d5f0378f9c7867092fa5939bb51
APPLE <mujeeblawal357@gmail.com>
2026-03-23 11:57:22 +0100

    fixes2

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,4 +1,4 @@
-import axiosInstance from "@/utils/axiosInstance";
+import axiosInstance from "../Utils/axiosInstance";
 
 export const registerUser = async (formData) => {
   return await axiosInstance.post("/auth/create", formData);
